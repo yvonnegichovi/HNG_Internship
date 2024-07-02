55a223153b943b9e993de9948233786a9a4ae439
yvonnegichovi <yvonnegichovi@gmail.com>
2024-07-03 00:30:52 +0300

    my application to be deployed

--- my-web-server/app.py
+++ my-web-server/app.py
@@ -8,20 +8,34 @@ import requests
 
 app = Flask(__name__)
 
+def get_location_from_ip(ip):
+    response = requests.get(f'http://ip-api.com/json/{ip}')
+    return response.json().get('city')
+
+def get_temperature(city):
+    api_key = 'YOUR_WEATHER_API_KEY'  # Replace with your actual weather API key
+    response = requests.get(f'http://api.weatherstack.com/current?access_key={api_key}&query={city}')
+    return response.json().get('current', {}).get('temperature')
 
 @app.route('/api/hello', methods=['GET'])
 def hello():
     visitor_name = request.args.get('visitor_name', 'Guest')
     client_ip = request.remote_addr
 
-    location = "New York"
-    temperature = 11
-    response = {
-        "client_ip": client_ip,
-        "location": location,
-        "greeting": f"Hello, {visitor_name}! The temperature is {temperature} degrees Celsius in {location}"
-    }
+    try:
+        location = get_location_from_ip(client_ip)
+        temperature = get_temperature(location)
+
+        response = {
+            "client_ip": client_ip,
+            "location": location,
+            "greeting": f"Hello, {visitor_name}! The temperature is {temperature} degrees Celsius in {location}"
+        }
+    except Exception as e:
+        response = {"error": str(e)}
+
     return jsonify(response)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
